00510e9ac76357cbdd027e1a153ef193049a73b2
rawenloft <ok.rawenloft@gmail.com>
2024-03-16 01:13:25 +0200

    Add variables.

--- main.go
+++ main.go
@@ -3,5 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World of Go!")
+	var conferenceName = "Go Conference"
+	var remainingTickets = 50
+	const conferenceTickets = 50
+
+	fmt.Printf("Welcome to %v booking application!\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v still available.\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend.")
+
+	var userName string
+	var userTickets int
+	// ask user for username
+	userName = "Tom"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+
 }
