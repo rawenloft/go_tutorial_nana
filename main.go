96ac1e99c477e9658deef01782176d818fc769fd
rawenloft <ok.rawenloft@gmail.com>
2024-03-16 01:48:40 +0200

    Add bookings array.

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ func main() {
 	var lastName string
 	var email string
 	var userTickets uint
+	var bookings [50]string
 	// ask user for username
 	fmt.Println("Please enter your first name: ")
 	fmt.Scan(&firstName)
@@ -27,11 +28,20 @@ func main() {
 
 	fmt.Println("Please enter number of tickets you like to purchase: ")
 	fmt.Scan(&userTickets)
+	if userTickets != 0 {
+		fmt.Printf("Thank you for purchasing %v tickets.\n", userTickets)
+		bookings[0] = firstName + " " + lastName
+	} else {
+		fmt.Printf("Sorry, you did not purchase any ticket!")
+	}
 
 	remainingTickets = remainingTickets - userTickets
+	fmt.Printf("The whole array: %v\n", bookings)
+	fmt.Printf("The first value: %v\n", bookings[0])
+	fmt.Printf("Array type: %T\n", bookings)
+	fmt.Printf("Array length: %v\n", len(bookings))
 
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will receie a confirmation email at %v.\n", firstName, lastName, userTickets, email)
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v.\n", firstName, lastName, userTickets, email)
 	fmt.Printf("We have total of %v tickets and %v tickets are still available.\n", conferenceTickets, remainingTickets)
-	fmt.Println("Get your tickets here to attend!")
 
 }
